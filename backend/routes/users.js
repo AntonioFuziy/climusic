31109b319947a88b57ba11e6423cf9ee567ec95b
AntonioFuziy <antoniovf@al.insper.edu.br>
2020-10-27 13:17:15 -0300

    conexão com mongoDB

---
+++ backend/routes/users.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+var User = require("../models/users.model");
+
+router.route("/").get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json("Error "+ err));
+});
+
+router.route("/add").post((req,res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User({username, email, password});
+
+    newUser.save()
+        .then(() => res.json("User added!"))
+        .catch(err => res.status(400).json("Error "+ err))
+});
+
+module.exports = router;
