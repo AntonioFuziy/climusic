7a6218576258ef2b761189072fb5d662f84eac4f
VergaraC <victorvaac@al.insper.edu.br>
2020-10-27 09:25:34 -0300

    Estrurura;'ao de pastas

---
+++ src/pages/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Weather = () => {
+    
+    
+    return(
+        <div className="weather">
+            <Link to="/">List</Link>
+        </div>
+    );
+}
+
+export default Weather;
