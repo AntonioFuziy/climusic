637b3c24a8ffce6745153ccd5619a3b5564de682
AntonioFuziy <antoniovf@al.insper.edu.br>
2020-11-04 22:13:37 -0300

    style weather

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -5,13 +5,15 @@ import { Link } from 'react-router-dom';
 import "../styles/Weather.css";
 
 import DefaultImage from '../images/default.jpg';
-import Clouds from '../images/cloudy.jpg';
-import Clear from '../images/clear.jpg';
-import Rain from '../images/rain.jpg';
-import Snow from '../images/snowing.jpg';
-import Drizzle from '../images/drizzle.jpg';
-import Thunderstorm from '../images/thunderstorm.jpg';
-import Foggy from '../images/foggy.jpg';
+import Clouds from '../images/cloudy.png';
+import Clear from '../images/sunny.png';
+import Rain from '../images/rain.png';
+import Snow from '../images/snowing.png';
+import Drizzle from '../images/drizzle.png';
+import Thunderstorm from '../images/storm.png';
+import Foggy from '../images/foggy.png';
+
+import logo from '../images/logo.png';
 
 import PlaylistTypes from '../parameters/playlists.json';
 
@@ -22,13 +24,15 @@ export default class Weather extends Component {
         this.state = {
             token: '',
             city : '',
-            weather: 'Busque sua cidade e veja recomendações de músicas',
+            weather: '',
             gender: '',
             images: [Clouds, Clear, Rain, Snow, Drizzle, Thunderstorm, Foggy],
             image: null,
             musics: [],
             playlist: '',
             types: null,
+            temperature: "",
+            city_name: ''
         }
           
         this.updateCity = this.updateCity.bind(this);
@@ -61,42 +65,62 @@ export default class Weather extends Component {
                 this.setState({
                     gender: "mood",
                     image: this.state.images[0],
-                    playlist: this.state.types.mood
+                    playlist: this.state.types.mood,
+                    weather: "Nublado"
                 });
                 break;
+                
             case "Clear":
                 this.setState({
                     gender: "happy",
                     image: this.state.images[1],
-                    playlist: this.state.types.happy
+                    playlist: this.state.types.happy,
+                    weather: "Limpo"
                 });
                 break;
+
             case "Rain":
                 this.setState({
                     gender: "mood",
                     image: this.state.images[2],
-                    playlist: this.state.types.mood
+                    playlist: this.state.types.mood,
+                    weather: "Chovendo"
                 });
                 break;
+
             case "Snow":
                 this.setState({
                     gender: "mood",
                     image: this.state.images[3],
-                    playlist: this.state.types.mood
+                    playlist: this.state.types.mood,
+                    weather: "Nevando"
                 });
                 break;
+
             case "Thunderstorm":
                 this.setState({
                     gender: "energy",
                     image: this.state.images[5],
-                    playlist: this.state.types.energy
+                    playlist: this.state.types.energy,
+                    weather: "Tempestade"
                 });
                 break;
+
             case "Drizzle":
                 this.setState({
                     gender: "mood",
                     image: this.state.images[4],
-                    playlist: this.state.types.mood
+                    playlist: this.state.types.mood,
+                    weather: "Chuviscando"
+                });
+                break;
+
+            case "Atmosphere":
+                this.setState({
+                    gender: "mood",
+                    image: this.state.images[6],
+                    playlist: this.state.types.mood,
+                    weather: "Neblina"
                 });
                 break;
 
@@ -112,11 +136,18 @@ export default class Weather extends Component {
         axios
         .get(API_URL)
         .then(response => {
+            console.log(response.data)
             var weather = response.data.weather[0].main
+            var temperature = response.data.main.temp
+            var city = response.data.name
 
             console.log(weather)
             
             this.getWeather(weather)
+            this.setState({
+                temperature: Math.round(temperature-273)+"°C",
+                city_name: city
+            })
             this.renderImage(weather)
             axios('https://accounts.spotify.com/api/token', {
                 headers: {
@@ -138,14 +169,10 @@ export default class Weather extends Component {
     render(){
         if (this.state.image != null){
             var climateImage = <img src={this.state.image} className="climate-image" alt=""/>
-        } else{
-            var climateImage = <img src={DefaultImage} className="climate-image" alt=""/>
-        }
+        } 
         if (this.state.playlist != ''){
             var widgetPlaylist = <iframe src={"https://open.spotify.com/embed/playlist/"+this.state.playlist} 
             className="player"
-            width="400" 
-            height="500" 
             frameBorder="0" 
             allowtransparency="true" 
             allow="encrypted-media">
@@ -153,8 +180,6 @@ export default class Weather extends Component {
         } else{
             var widgetPlaylist = <iframe src={"https://open.spotify.com/embed/playlist/4QkCEiUjKbhc3mSzQo6ycO"} 
             className="player"
-            width="400" 
-            height="500" 
             frameBorder="0" 
             allowtransparency="true" 
             allow="encrypted-media">
@@ -165,6 +190,7 @@ export default class Weather extends Component {
             <div className="container">
                 <div className="box">
                     <form className="city-form">
+                        <img src={logo} className="logo" alt=""/>
                         <div className="form-group">
                             <label htmlFor="cidade-input">Qual cidade você está nesse momento?</label>
                             <input id="cidade-input" className="form-control" type="text" onChange={this.updateCity} name="city"/>
@@ -174,11 +200,13 @@ export default class Weather extends Component {
                     </form>
 
                     <div className="weather">
-                        <p className="weather-description">{this.state.weather}</p>
-                        <p>{this.state.gender}</p>
-
-                        {climateImage}
-
+                        <div className="weather-box">
+                            {climateImage}
+                            <h3 className="city-title">{this.state.city_name}</h3>
+                            <p className="weather-description">{this.state.weather}</p>
+                            <p className="temperature">{this.state.temperature}</p>
+                        </div>
+                        {/* <p className="gender">{this.state.gender}</p> */}
                         {widgetPlaylist}
                         {/* <ul className="musics">
                             {this.state.musics.map((item, index) => 
